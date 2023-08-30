691207661cc2cbb872dba6fb401bb6f152865c61
Omid <Omid_nikzad@hotmail.se>
2023-08-30 14:36:06 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Fregment_23_08_23"
+include(":app")
+ 
